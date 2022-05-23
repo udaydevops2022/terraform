c29b8c2e3a0224f9ace726c6e05beed7739b8d69
uday <udaydevops2022@gmail.com>
2022-05-23 14:50:04 +0530

    added all

---
+++ input.tf
@@ -0,0 +1,30 @@
+variable "region" {
+    type        = string
+    default  = "ap-southeast-1"
+}
+variable "nw_vpc_cidr" {
+    type = string
+    default = "10.0.0.0/16"
+  
+}
+
+variable "subnet_cidrs" {
+    type = list(string)
+    default = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
+  
+}
+variable "aws_zone_a" {
+    type = string
+    default = "ap-southeast-1a"
+  
+}
+variable "aws_zone_b" {
+    type = string
+    default = "ap-southeast-1b"
+  
+}
+variable "aws_s3_bucket" {
+    type = string
+    default = "exs3bucket"
+  
+}
---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+    region = var.region
+    access_key  =   "AKIAW4GAKVILP3VGL2MF"
+    secret_key  =   "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"
+  
+}
