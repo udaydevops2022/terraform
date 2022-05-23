4ad455b463761be6fcb8383fe5604a5f12f2cfac
uday <udaydevops2022@gmail.com>
2022-05-23 13:18:01 +0530

    added fix

--- excercise/input.tf
+++ excercise/input.tf
@@ -9,7 +9,7 @@ variable "nw_vpc_cidr" {
 }
 
 variable "subnet_cidrs" {
-    type = list[string]
+    type = list(string)
     default = "10.0.0.0/24, 10.0.1.0/24, 10.0.2.0/24, 10.0.3.0/24, 10.0.4.0/24, 10.0.5.0/24"
   
 }
